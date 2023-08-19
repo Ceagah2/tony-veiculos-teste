98f43284e96698bbfd67ff9aea3cdb4576c07f51
Ceagah2 <carlosceagah@gmail.com>
2023-08-19 12:03:13 -0300

    type: create card types

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -7,12 +7,22 @@ export interface VehicleProps {
   fabricationYear: number;
   fuelType: string;
   id: number;
+  make: string;
   model: string;
   modelYear: number;
+  numberDoors: number;
   optional: string[];
   photos: [];
   price: number;
   transmission: string;
   vehicleType: string;
   version: string;
+}
+
+export interface CardProps {
+  image: string;
+  make: string;
+  model: string;
+  modelYear: number;
+  price: number;
 }
