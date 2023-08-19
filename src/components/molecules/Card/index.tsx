de5ab39366a3f30fcadd3c280ca4c3b22a0caf37
Ceagah2 <carlosceagah@gmail.com>
2023-08-19 16:50:40 -0300

    feat: working on cars card

--- src/components/molecules/Card/index.tsx
+++ src/components/molecules/Card/index.tsx
@@ -1,7 +1,60 @@
-import { type CardProps } from '@/@types';
+import { VehicleProps } from '@/@types';
+import { AiOutlineCar } from 'react-icons/ai';
+import { BiGasPump } from 'react-icons/bi';
+import { BsCalendarWeek } from 'react-icons/bs';
+import {
+  CardContent,
+  CardDescription,
+  CardDetailText,
+  CardHeader,
+  CardImage,
+  CardImageContainer,
+  CardLineDetailsDiv,
+  CardLineHeaderDiv,
+  CardPrice,
+  CardSubTitle,
+  CardTitle,
+  Container,
+} from './styles';
 
-const Card = (props: CardProps) => {
-  console.log(props);
+const Card = (props: VehicleProps) => {
+  const priceWithoutDecimal = props.price.toString().slice(0, -2);
+  const newPrice = parseFloat(priceWithoutDecimal);
+  const formattedPrice = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(newPrice);
+  return (
+    <Container>
+      <CardImageContainer>
+        <CardImage src={props.photos} />
+      </CardImageContainer>
+      <CardContent>
+        <CardHeader>
+          <CardLineHeaderDiv>
+            <CardTitle>{props.make}</CardTitle>
+            <CardSubTitle>{props.model}</CardSubTitle>
+          </CardLineHeaderDiv>
+        </CardHeader>
+        <CardDescription>{props.fipeId}</CardDescription>
+        <CardPrice>{formattedPrice}</CardPrice>
+        <CardLineDetailsDiv>
+          <CardDetailText>
+            <BsCalendarWeek />
+            {props.fabricationYear}/{props.modelYear}
+          </CardDetailText>
+          <CardDetailText>
+            <BiGasPump />
+            {props.fuelType}
+          </CardDetailText>
+          <CardDetailText>
+            <AiOutlineCar />
+            {props.kmVehicle}
+          </CardDetailText>
+        </CardLineDetailsDiv>
+      </CardContent>
+    </Container>
+  );
 };
 
 export default Card;
--- src/components/molecules/Card/styles.ts
+++ src/components/molecules/Card/styles.ts
@@ -1,3 +1,85 @@
+import { theme } from '@/styles/theme';
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 30vw;
+  height: 200px;
+  display: flex;
+  flex-direction: row;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: margin-top 0.5s, box-shadow 0.5s;
+  box-shadow: 0px 1px 0px 0px #000;
+
+  &:hover {
+    margin-top: -20px;
+    box-shadow: 0px 5px 20px 0px #000;
+  }
+`;
+export const CardImageContainer = styled.div`
+  width: 200px;
+  height: 100%;
+  border-radius: 20px;
+`;
+export const CardImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 20px;
+`;
+export const CardContent = styled.div`
+  width: 60%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding-left: 10px;
+`;
+export const CardHeader = styled.div`
+  width: 100%;
+  padding: 10px;
+`;
+export const CardLineHeaderDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+export const CardTitle = styled.h1`
+  padding-right: 10px;
+  font-family: ${theme.fonts.heading};
+  font-size: ${theme.fontSizes.subHeading};
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+export const CardSubTitle = styled.p`
+  font-family: ${theme.fonts.subHeading};
+  font-size: ${theme.fontSizes.body};
+  text-transform: uppercase;
+`;
+export const CardDescription = styled.span`
+  text-transform: capitalize;
+`;
+export const CardPrice = styled.span`
+  align-self: flex-start;
+  font-family: ${theme.fonts.subHeading};
+  font-size: ${theme.fontSizes.body};
+  color: ${theme.colors.blue};
+  font-weight: ${theme.fontWeights.bold};
+`;
+export const CardLineDetailsDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  padding: 10px;
+`;
+export const CardDetailText = styled.span`
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes.description};
+  text-align: center;
+  display: flex;
+`;
+export const CardOptions = styled.span``;
