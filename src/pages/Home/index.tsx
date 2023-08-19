6f216d0c6bcb3dd8f02f12f99e39501c2b63e30c
Ceagah2 <carlosceagah@gmail.com>
2023-08-19 12:03:55 -0300

    feat: fetch data and treat erros from api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,40 @@
+import { type VehicleProps } from '@/@types';
 import Container from '@/components/atoms/Container';
 import Header from '@/components/atoms/Header';
+import { Api } from '@/services/api';
+import { useEffect, useState } from 'react';
+import { CardsContainer, Content, ContentHeader } from './styles';
 
 const Home = () => {
+  const LIMIT = 30;
+  const [carsData, setCarsData] = useState([]);
+  const [errorMessage, setErrorMessage] = useState<boolean>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await Api.get(`/vehicles?limit=${LIMIT}`).then((response) => {
+        if (response.data.message === 'Vehicles found successfully') {
+          const filteredCars = response.data.vehicles.filter(
+            (vehicle: VehicleProps) => vehicle.vehicleType === 'car',
+          );
+          setCarsData(filteredCars);
+        } else {
+          setErrorMessage(true);
+        }
+      });
+    };
+    fetchData();
+  }, []);
+
+  console.log(carsData);
+
   return (
     <Container>
       <Header />
-      <h1>Hello Tony</h1>
+      <Content>
+        <ContentHeader>Hello Tony Header</ContentHeader>
+        <CardsContainer>{errorMessage && <h1>Error</h1>}</CardsContainer>
+      </Content>
     </Container>
   );
 };
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Content = styled.section`
+  width: 100%;
+  height: calc(100vh - 60px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const ContentHeader = styled.header``;
+export const CardsContainer = styled.section``;
