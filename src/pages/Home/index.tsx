121c89f81b8648ccfc66cb90c44e57f8483f77f1
Ceagah2 <carlosceagah@gmail.com>
2023-08-19 16:50:54 -0300

    feat: sanitize data from the api

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,45 @@
 import { type VehicleProps } from '@/@types';
 import Container from '@/components/atoms/Container';
 import Header from '@/components/atoms/Header';
+import Card from '@/components/molecules/Card';
+import NoImage from '@/public/noImage.png';
 import { Api } from '@/services/api';
 import { useEffect, useState } from 'react';
 import { CardsContainer, Content, ContentHeader } from './styles';
 
 const Home = () => {
   const LIMIT = 30;
+  const OFFSET = 60; // esse offset tem menos carros sem imagem
   const [carsData, setCarsData] = useState([]);
   const [errorMessage, setErrorMessage] = useState<boolean>();
 
   useEffect(() => {
     const fetchData = async () => {
-      await Api.get(`/vehicles?limit=${LIMIT}`).then((response) => {
-        if (response.data.message === 'Vehicles found successfully') {
-          const filteredCars = response.data.vehicles.filter(
-            (vehicle: VehicleProps) => vehicle.vehicleType === 'car',
-          );
-          setCarsData(filteredCars);
-        } else {
-          setErrorMessage(true);
-        }
-      });
+      await Api.get(`/vehicles?limit=${LIMIT}&offset=${OFFSET}`).then(
+        (response) => {
+          if (response.data.message === 'Vehicles found successfully') {
+            const filteredCars = response.data.vehicles.filter(
+              (vehicle: VehicleProps) => vehicle.vehicleType === 'car',
+            );
+
+            const modifiedCarsData = filteredCars.map(
+              (vehicle: VehicleProps) => {
+                if (vehicle.make === 'VW - VOLKSWAGEN') {
+                  return { ...vehicle, make: 'VW' };
+                } else if (vehicle.make === 'GM - CHEVROLET') {
+                  return { ...vehicle, make: 'GM' };
+                }
+
+                return vehicle;
+              },
+            );
+
+            setCarsData(modifiedCarsData);
+          } else {
+            setErrorMessage(true);
+          }
+        },
+      );
     };
     fetchData();
   }, []);
@@ -32,8 +50,30 @@ const Home = () => {
     <Container>
       <Header />
       <Content>
-        <ContentHeader>Hello Tony Header</ContentHeader>
-        <CardsContainer>{errorMessage && <h1>Error</h1>}</CardsContainer>
+        <ContentHeader>
+          Encontre aqui os melhores carros disponíveis no mercado.
+        </ContentHeader>
+        <CardsContainer>
+          {errorMessage ? (
+            <h1>Error</h1>
+          ) : (
+            carsData.map((car: VehicleProps) => (
+              <Card
+                photos={car.photos ? car.photos[0].url : NoImage}
+                key={car.id}
+                make={car.make}
+                model={car.model}
+                modelYear={car.modelYear}
+                price={car.price}
+                optional={car.optional}
+                fipeId={car.fipeId}
+                fabricationYear={car.fabricationYear}
+                fuelType={car.fuelType}
+                kmVehicle={car.kmVehicle}
+              />
+            ))
+          )}
+        </CardsContainer>
       </Content>
     </Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -8,5 +8,22 @@ export const Content = styled.section`
   align-items: center;
   justify-content: center;
 `;
-export const ContentHeader = styled.header``;
-export const CardsContainer = styled.section``;
+export const ContentHeader = styled.header`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const CardsContainer = styled.section`
+  width: 100%;
+  height: 90%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  padding: 20px;
+`;
