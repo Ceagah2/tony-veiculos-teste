f85312e757d010ccc550ed5e58de3e404c3fee2b
Ceagah2 <carlosceagah@gmail.com>
2023-08-19 10:45:18 -0300

    style: create theme file

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    white: '#fff',
+    black: '#000',
+    Blue: '#0074D9',
+    Red: 'FF4136',
+    LightGray: '#F0F0F0',
+
+    success: '#2ecc71',
+    error: '#e74c3c',
+    warning: '#f1c40f',
+  },
+  fonts: {
+    heading: 'Source Code Pro',
+    subHeading: 'Open Sans',
+    body: 'Roboto',
+  },
+  fontSizes: {
+    heading: '1.5rem',
+    subHeading: '1.25rem',
+    body: '1rem',
+    description: '0.75rem',
+  },
+  fontWeights: {
+    bold: 700,
+    semiBold: 600,
+    normal: 400,
+  },
+};
