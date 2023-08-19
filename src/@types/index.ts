cc8daf7dd8ca561e39fd71b530a70cb57c7451fd
Ceagah2 <carlosceagah@gmail.com>
2023-08-19 11:53:05 -0300

    docs: create vehicles type

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,18 @@
 export interface ContainerProps {
   children: React.ReactNode;
 }
+
+export interface VehicleProps {
+  color: string;
+  fabricationYear: number;
+  fuelType: string;
+  id: number;
+  model: string;
+  modelYear: number;
+  optional: string[];
+  photos: [];
+  price: number;
+  transmission: string;
+  vehicleType: string;
+  version: string;
+}
