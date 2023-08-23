6603f7a6af1dad9c40792c12d14d9a04c60428a6
Ceagah2 <carlosceagah@gmail.com>
2023-08-22 21:31:51 -0300

    feat: create alert component

---
+++ src/components/molecules/Alert/index.tsx
@@ -0,0 +1,25 @@
+import { type AlertProps } from '@/@types';
+import Button from '@/components/atoms/Button';
+import {
+  AlertText,
+  AlertTitle,
+  ButtonContainer,
+  Container,
+  Content,
+} from './styles';
+
+const Alert = (props: AlertProps) => {
+  return (
+    <Container>
+      <Content isOpen={props.isOpen}>
+        <AlertTitle>{props.title}</AlertTitle>
+        <AlertText>{props.text}</AlertText>
+        <ButtonContainer>
+          <Button onClick={props.onClose} text="Ok" />
+        </ButtonContainer>
+      </Content>
+    </Container>
+  );
+};
+
+export default Alert;
---
+++ src/components/molecules/Alert/styles.ts
@@ -0,0 +1,82 @@
+import { theme } from '@/styles/theme';
+import styled, { css, keyframes } from 'styled-components';
+
+interface ContainerProps {
+  isOpen: boolean;
+}
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  z-index: 9;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+export const Content = styled.div<ContainerProps>`
+  width: 40%;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  background-color: ${theme.colors.lightGray};
+  border: none;
+  border-radius: 20px;
+  position: fixed;
+  top: ${({ isOpen }) => (isOpen ? '30%' : '-150px')};
+  left: 30%;
+  transition: top 0.3s ease-in-out;
+
+  ${({ isOpen }) =>
+    isOpen &&
+    css`
+      animation: ${slideIn} 0.3s forwards;
+    `}
+
+  ${({ isOpen }) =>
+    !isOpen &&
+    css`
+      animation: ${slideOut} 0.3s forwards;
+    `}
+`;
+
+export const AlertTitle = styled.h1`
+  color: ${theme.colors.black};
+  font-size: ${theme.fontSizes.heading};
+  font-weight: ${theme.fontWeights.bold};
+  text-align: center;
+`;
+
+export const AlertText = styled.p`
+  color: ${theme.colors.black};
+  font-size: ${theme.fontSizes.body};
+  text-align: center;
+  font-weight: ${theme.fontWeights.normal};
+`;
+export const ButtonContainer = styled.div`
+  width: 50%;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  border-radius: 30px;
+  border: none;
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+`;
+
+const slideOut = keyframes`
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(-100%);
+  }
+`;
