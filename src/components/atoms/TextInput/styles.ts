f774696bd09d55550aedb622b150c7ecbdd1b1b0
Ceagah2 <carlosceagah@gmail.com>
2023-08-22 21:32:22 -0300

    feat: finish input styling

--- src/components/atoms/TextInput/styles.ts
+++ src/components/atoms/TextInput/styles.ts
@@ -1,5 +1,24 @@
+import { theme } from '@/styles/theme';
 import styled from 'styled-components';
 
-export const Container = styled.div``;
-export const InputLabel = styled.label``;
-export const Input = styled.input``;
+export const Container = styled.div`
+  width: 100%;
+  height: 75px;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+export const InputLabel = styled.label`
+  font-family: ${theme.fonts.body};
+  font-size: ${theme.fontSizes.description};
+  color: ${theme.colors.darkGray};
+  font-weight: ${theme.fontWeights.bold};
+`;
+export const Input = styled.input`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  border: 1px solid ${theme.colors.gray};
+  padding-left: 10px;
+`;
