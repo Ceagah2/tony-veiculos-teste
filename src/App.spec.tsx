c03315136ba93f8b390ad071da7ca67868e8f5d6
Ceagah2 <carlosceagah@gmail.com>
2023-08-23 21:59:28 -0300

    test: create Home screen tests

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,10 +1,25 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Home from './pages/Home';
 
 describe('App Component, Home screen', () => {
-  test('Should fetch data and check the error message ', () => {
-    const { getByTestId } = render(<Home />);
-    const errorText = getByTestId('error-message');
-    expect(errorText.childElementCount).toBe(0);
+  test('Should fetch data and check if loader is on ', () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const Loader = getByTestId('loader');
+    expect(Loader.childElementCount).toBe(4);
+  });
+
+  test('Should render a list of cars', () => {
+    const { getByTestId } = render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const CardsContainer = getByTestId('cards-container');
+    expect(CardsContainer.childElementCount).toBeGreaterThan(0);
   });
 });
